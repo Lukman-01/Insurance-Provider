e17e26327bea67312b99fc7703f4dad2d56f9297
Lukman-01 <abdulyekeenlukman@gmail.com>
2023-12-26 10:12:40 +0100

    test

---
+++ test/WalletFactoryTest.js
@@ -0,0 +1,56 @@
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
+
+describe("WalletFactory", function () {
+  let walletFactory;
+  let verifierCompany;
+  let owner;
+  let signer2;
+  let signer3;
+  let contractAddress;
+
+  before(async function () {
+    [owner, signer2, signer3] = await ethers.getSigners();
+
+    const WalletFactory = await ethers.getContractFactory("WalletFactory");
+    walletFactory = await WalletFactory.deploy(owner.address);
+
+    verifierCompany = owner.address;
+  });
+
+  it("should create a new wallet contract", async function () {
+    await walletFactory.createWallet();
+
+    contractAddress = await walletFactory.getWalletByOwner(owner.address);
+    const contract = await ethers.getContractAt("Wallet", contractAddress);
+
+    expect(contract).to.exist;
+    expect(await contract.verifierCompany()).to.equal(verifierCompany);
+  });
+
+  it("should return the correct contract address for an owner", async function () {
+    const retrievedContractAddress = await walletFactory.getWalletByOwner(owner.address);
+
+    expect(retrievedContractAddress).to.equal(contractAddress);
+  });
+
+  it("should return the list of deployed wallet contracts", async function () {
+    const deployedContracts = await walletFactory.getAllDeployedWallets();
+
+    expect(deployedContracts).to.deep.equal([contractAddress]);
+  });
+
+  it("should add a new user's wallet contract to the list of deployed contracts", async function () {
+  
+    await walletFactory.connect(signer3).createWallet();
+  
+    const deployedContracts = await walletFactory.getAllDeployedWallets();
+  
+    expect(deployedContracts).to.have.lengthOf(2);
+    expect(deployedContracts).to.include(contractAddress);
+  
+    const newContractAddress = await walletFactory.getWalletByOwner(signer3.address);
+    expect(deployedContracts).to.include(newContractAddress);
+  });
+  
+});
