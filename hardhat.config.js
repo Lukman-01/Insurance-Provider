8c07bb368abfbbadea06f1328ea0ae8857ccdeb4
Lukman-01 <abdulyekeenlukman@gmail.com>
2023-12-26 10:13:06 +0100

    configfile

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,19 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("hardhat-deploy");
+require("dotenv").config({ path: ".env" });
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const ALCHEMY_HTTP_URL = process.env.ALCHEMY_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 
 module.exports = {
   solidity: "0.8.19",
   networks: {
-    hardhat: {
-      chainId: 1337, // Chain ID of the Hardhat local blockchain
+    sepolia: {
+      url: ALCHEMY_HTTP_URL,
+      accounts: [PRIVATE_KEY],
     },
-  },
-  paths: {
-    artifacts: "./artifacts",
-    cache: "./cache",
-    sources: "./contracts",
-    tests: "./test",
   },
 };
