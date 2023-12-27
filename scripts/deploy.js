9c48976e7e9e5af46bba9c9352955b39ae4a1765
Lukman-01 <abdulyekeenlukman@gmail.com>
2023-12-27 07:58:04 +0100

    deploy all contracts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,24 +2,40 @@ const hre = require("hardhat");
 const { ethers } = require("hardhat");
 
 async function main() {
-const [deployer] = await ethers.getSigners();
+    const [deployer] = await ethers.getSigners();
 
-// Deploy the StorageVictim contract
-console.log("Deploying StorageVictimResolved with the account:", deployer.address);
-const StorageVictimResolved = await ethers.getContractFactory("StorageVictimResolved");
-const storageVictimResolved = await StorageVictimResolved.deploy();
+    console.log("Deploying contracts with the account:", deployer.address);
 
-//await storageVictimResolved.deploy();
+    // Deploy the InsuranceFactory contract
+    const InsuranceFactory = await ethers.getContractFactory("InsuranceFactory");
+    const insuranceFactory = await InsuranceFactory.deploy(deployer.address);
+    await insuranceFactory.deployed();
+    console.log("InsuranceFactory contract deployed to:", insuranceFactory.address);
 
-console.log("StorageVictimResolved contract deployed to:", storageVictimResolved.address);
+    // Create an Insurance contract instance through the InsuranceFactory
+    const createInsuranceTx = await insuranceFactory.createInsurance();
+    await createInsuranceTx.wait();
+    const deployedInsuranceAddresses = await insuranceFactory.getDeployedInsuranceContracts();
+    console.log("Deployed Insurance contract address:", deployedInsuranceAddresses[0]);
 
-console.log("Deployment completed!");
+    // Deploy the WalletFactory contract
+    const WalletFactory = await ethers.getContractFactory("WalletFactory");
+    const walletFactory = await WalletFactory.deploy(deployer.address);
+    await walletFactory.deployed();
+    console.log("WalletFactory contract deployed to:", walletFactory.address);
+
+    // Create a Wallet contract instance through the WalletFactory
+    const createWalletTx = await walletFactory.createWallet();
+    await createWalletTx.wait();
+    const deployedWalletAddresses = await walletFactory.getAllDeployedWallets();
+    console.log("Deployed Wallet contract address:", deployedWalletAddresses[0]);
+
+    console.log("Deployment completed!");
 }
 
 main()
-.then(() => process.exit(0))
-.catch((error) => {
-console.error(error);
-process.exit(1);
-});
-
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
